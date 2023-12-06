6543446525e536a279280703bb115dee8e2aeda8
Pleo2/Jose Moreno <leooel23m@gmail.com>
2023-12-06 09:05:54 -0400

    add TypeScript code and index.ts minute:52:53

---
+++ index.ts
@@ -0,0 +1,11 @@
+console.log('hello world')
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(module)
+// console.log(require)
+
+// console.log(process)
+
+setInterval(() => console.log('helloWorld'), 2000)
+setTimeout(() => console.log('helloWorld set time out'), 500)
